2e9470dca67f5289927c04248116a14280f01eec
HugIII <hugo@blayes.fr>
2024-11-04 17:29:12 +0100

    test ok

--- test_task.py
+++ test_task.py
@@ -6,7 +6,7 @@ import task
 class TestTask(unittest.TestCase):
     def test(self):
         t = task.Task()
-        # t.work()
+        t.work()
         numpy.testing.assert_allclose(numpy.dot(t.a, t.x), t.b)
 
 
