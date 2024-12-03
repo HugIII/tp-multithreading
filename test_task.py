89bb5435f9be6eceb04556ae9b484e32c44bf59c
HugIII <hugi@vma.myguest.virtualbox.org>
2024-12-03 13:42:25 +0100

    change test

--- test_task.py
+++ test_task.py
@@ -1,16 +1,16 @@
 import unittest
-import numpy
+import numpy as np
 import task
 
 
 class TestTask(unittest.TestCase):
     def test(self):
-    	size = np.random.randint(300, 3_000)
-	a = np.random.rand(size, size)
+        size = np.random.randint(300, 3_000)
+        a = np.random.rand(size, size)
         b = np.random.rand(size)
-        t = task.Task(a,b)
+        t = task.Task(a, b)
         t.work()
-        numpy.testing.assert_allclose(numpy.dot(t.a, t.x), t.b)
+        np.testing.assert_allclose(np.dot(t.a, t.x), t.b)
 
 
 if __name__ == "__main__":
